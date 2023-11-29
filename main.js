41079034f47707ac6bb9f348a8f5e9441f0da80d
Md Minhaj <parsonal322532@gmail.com>
2023-11-29 16:43:22 +0600

    Filter By Lnaguage Added

--- main.js
+++ main.js
@@ -13,23 +13,54 @@ const showDisplay = (data)=>{
         newElement.innerHTML = `
             <img class="w-full h-40" src="${element.flags.png}" alt="">
             <h2 class="my-2">Conutry Name: ${element.name.common}</h2>
-            <h3 class="mb-2">Capital City: ${element.capital[0]}</h3>
+            <h3 class="mb-2">Capital City: ${element.capital ? element.capital[0] : 'No Capital'}</h3>
             <h4>Region: ${element.region}</h4>
         `;
-        console.log(element);
 
         countryContainer.appendChild(newElement)
     });
+}
+
+const languageData = () =>{
+    fetch(`https://restcountries.com/v3.1/all`)
+    .then(res => res.json())
+    .then(data => languageShow(data))
+}
+
+const languageShow = (data) =>{
+    const langContainer = document.getElementById('lang-container');
+    const newArrayValue = [];
+    data.forEach(element =>{
+        if(element.languages !== undefined){
+            newArrayValue.push(Object.values(element.languages)[0]);
+        }
+    })
+    const languageData = [];
+    for(const filterArray of newArrayValue){
+        if(languageData.includes(filterArray) === false){
+            languageData.push(filterArray);
+        }
+    }
+    languageData.forEach(element =>{
+        const newElement = document.createElement('li');
+        newElement.classList.add('cursor-pointer', 'w-100')
+        newElement.innerHTML = `
+        <button class="text-lg py-2 text-start hover:text-red-500 ">${element}</button>
+        `;
+        langContainer.appendChild(newElement);
+        
+    })
 }
 
 const regionClick = (region) =>{
     loadData(region);
 }
 
-const filterRegion = () =>{
-    const filterRegion = document.getElementById('region');
+const filterRegion = (elementId) =>{
+    const filterRegion = document.getElementById(elementId);
     filterRegion.classList.toggle('hidden');
 }
 
 
+languageData();
 loadData('africa');
